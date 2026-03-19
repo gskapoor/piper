905e362d8464d443ad8b0d6dfc67e4c1b5f8b91c
Gobind Kapoor <kapoorgobind@gmail.com>
2026-03-19 00:49:44 -0400

    added buffer drawing

--- src/piper.c
+++ src/piper.c
@@ -1,12 +1,38 @@
 #include <stdio.h>
+#include <stdint.h>
+
 #include "raylib.h"
 
+#define SQUARE_SIZE 256
+
+// Using standard int since calling raylib
+static const int screenWidth = 1024;
+static const int screenHeight = 1024;
+
+// TODO: add details of how I'm storing this
+// For now, first byte represents number of entries, except for "0" which represents a straight pipe
+// Second byte is orientation, indexed by the counterclockwise most open pipe.
+// 0 is up, 1 is right, 2 is down and 3 is left.
+// For straight pipes, 0 is up/down, 1 is right/left
+static uint8_t tiles[4][4] = {
+    {0x11, 0x31, 0x12, 0x12},
+    {0x21, 0x23, 0x00, 0x00},
+    {0x00, 0x21, 0x23, 0x00},
+    {0x11, 0x33, 0x01, 0x23}
+};
+
 int main(int argc, char** argv){
-    InitWindow(200, 200, "Piper");
+    InitWindow(screenWidth, screenHeight, "Piper");
 
     BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("I'm a piper : )", 0, 0, 5, ORANGE);
+        for (int i = 0; i < 4; i++){
+            for (int j = 0; j < 4; j++){
+                char buf[1];
+                sprintf(buf,"%d", tiles[i][j]);
+                DrawText(buf, i * SQUARE_SIZE, j * SQUARE_SIZE, 5, ORANGE);
+            }
+        }
     EndDrawing();
 
     while(!WindowShouldClose()){
