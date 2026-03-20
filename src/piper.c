17f47ed1c1811a0f880b096954179f88fb6f482d
Gobind Kapoor <kapoorgobind@gmail.com>
2026-03-19 23:40:21 -0400

    generating pipes

--- src/piper.c
+++ src/piper.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 #include "raylib.h"
 
@@ -18,12 +19,52 @@ static const int pipeWidth = SQUARE_SIZE / 4;
 static const int pipeOffset = pipeWidth / 2;
 
 // TODO: add details of how I'm storing this
-static uint8_t tiles[4][4] = {
-    {0b0010, 0b1110, 0b1100, 0b0100},
-    {0b0110, 0b1001, 0b0101, 0b0101},
-    {0b0001, 0b0110, 0b1001, 0b0101},
-    {0b0010, 0b1011, 0b1010, 0b1001}
-};
+// static uint8_t tiles[4][4] = {
+//     {0b0010, 0b1110, 0b1100, 0b0100},
+//     {0b0110, 0b1001, 0b0101, 0b0101},
+//     {0b0001, 0b0110, 0b1001, 0b0101},
+//     {0b0010, 0b1011, 0b1010, 0b1001}
+// };
+
+// Tile Related Functions
+
+bool tile_has_single_entry(uint8_t tile){
+    return tile == NORTH || tile == EAST || tile == SOUTH || tile == WEST;
+}
+
+bool tile_has_bent_pipe(uint8_t tile){
+    return (
+        tile == (NORTH | EAST) ||
+        tile == (SOUTH | EAST) ||
+        tile == (NORTH | WEST) ||
+        tile == (WEST | SOUTH)
+    );
+}
+
+bool tile_has_straight_pipe(uint8_t tile){
+    return (
+        tile == (NORTH | SOUTH) || tile == (WEST | EAST)
+    );
+}
+
+bool tile_has_triple_entry(uint8_t tile){
+    return (
+        tile == (NORTH | EAST | WEST) ||
+        tile == (SOUTH | EAST | WEST) ||
+        tile == (NORTH | WEST | SOUTH) ||
+        tile == (NORTH | EAST | SOUTH)
+    );
+}
+
+// Tilemap creation
+// Assume an "empty" tilemap is calloced to 0
+void generate_tilemap(uint8_t** tilemap, uint8_t m, uint8_t n){
+    for (int row = 0; row < m; row++){
+        for (int col = 0; col < n; col++){
+            tilemap[row][col] = NORTH | SOUTH;
+        }
+    }
+}
 
 uint8_t rotate_tile_once(uint8_t tile){
     // Counter Clockwise
@@ -32,7 +73,7 @@ uint8_t rotate_tile_once(uint8_t tile){
     return ((tile << 1) & 0xF) + (tile >> 3);
 }
 
-void redraw_pipes(void){
+void redraw_pipes(uint8_t** tiles){
     BeginDrawing();
         ClearBackground(RAYWHITE);
         for (int i = 0; i < 4; i++){
@@ -69,7 +110,16 @@ void redraw_pipes(void){
 int main(int argc, char** argv){
     InitWindow(screenWidth, screenHeight, "Piper");
 
-    redraw_pipes();
+    uint8_t m = 4, n = 4;
+
+    uint8_t **tiles = malloc(m * sizeof(uint8_t *));
+    for (int i = 0; i < m; i++)
+        tiles[i] = malloc(n * sizeof(uint8_t));
+
+
+    generate_tilemap(tiles, 4, 4);
+
+    redraw_pipes(tiles);
 
     while(!WindowShouldClose()){
         PollInputEvents();
@@ -84,7 +134,7 @@ int main(int argc, char** argv){
 
             tiles[row][col] = rotate_tile_once(tiles[row][col]);
 
-            redraw_pipes();
+            redraw_pipes(tiles);
         }
     }
     CloseWindow();
