8e7e5ec036c9b5c1933ef140333c970f44e5a7ef
Gobind Kapoor <kapoorgobind@gmail.com>
2026-04-12 17:52:32 -0400

    added definitions for the prio queue

--- src/priority_queue.h
+++ src/priority_queue.h
@@ -1,6 +1,16 @@
 #ifndef PRIORITY_QUEUE_H
 #define PRIORITY_QUEUE_H
 
+#include "array.h"
+#include <stdint.h>
 
+typedef struct {
+    void* item;
+    uint32_t weight;
+} prio_nodes;
+
+typedef struct {
+    array arr;
+} prio_queue;
 
 #endif
